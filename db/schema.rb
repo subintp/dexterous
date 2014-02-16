dc5b38415d4b3e696733be2aeb92e13e744cc9d1
lorefnon <lorefnon@gmail.com>
2014-02-16 22:47:39 +0530

    Test track model attributes

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140216160518) do
+ActiveRecord::Schema.define(version: 20140216162243) do
+
+  create_table "tracks", force: true do |t|
+    t.integer  "creator_id"
+    t.string   "title"
+    t.text     "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
--- spec/models/track_spec.rb
+++ spec/models/track_spec.rb
@@ -1,5 +1,13 @@
 require 'spec_helper'
 
 describe Track do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe "attributes" do
+    it "has title, description attributes" do
+      %w{title description}.each do |field|
+        expect(Track.attribute_names).to include field
+      end
+    end
+  end
+
 end
