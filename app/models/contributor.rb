06e2bfb368998860f9648ab5d227e5a4f91dc2db
lorefnon <lorefnon@gmail.com>
2014-02-17 00:37:46 +0530

    Backfill lower order permissions in contributor model

--- app/models/contributor.rb
+++ app/models/contributor.rb
@@ -1,5 +1,15 @@
 class Contributor < ActiveRecord::Base
+
   belongs_to :target, polymorphic: true
   belongs_to :user
   belongs_to :associator, class_name: 'User'
+  before_save :backfill_lower_permissions
+
+  private
+
+  def backfill_lower_permissions
+    self.can_edit ||= is_admin
+    self.can_read ||= can_edit
+  end
+
 end
--- spec/models/contributor_spec.rb
+++ spec/models/contributor_spec.rb
@@ -4,4 +4,10 @@ describe Contributor do
   it { is_expected.to belong_to :user }
   it { is_expected.to belong_to(:associator).class_name('User') }
   it { is_expected.to belong_to :target }
+
+  it "can_read if (s)he can_edit" do
+    track = create :track_contributor, can_edit: true
+    expect(track.can_read).to be true
+  end
+
 end
