dc0d15c3ac0c81a533868550d027ad2d64a722a8
lorefnon <lorefnon@gmail.com>
2014-02-16 22:30:01 +0530

    Define schema for track

--- db/migrate/20140216162243_create_tracks.rb
+++ db/migrate/20140216162243_create_tracks.rb
@@ -1,6 +1,9 @@
 class CreateTracks < ActiveRecord::Migration
   def change
     create_table :tracks do |t|
+      t.integer :creator_id
+      t.string :title
+      t.text :description
 
       t.timestamps
     end
