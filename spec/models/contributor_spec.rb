42d646c016f11a94dc07c5812a83e96f715c4014
lorefnon <lorefnon@gmail.com>
2014-02-16 23:43:16 +0530

    Improve tests

--- spec/models/contributor_spec.rb
+++ spec/models/contributor_spec.rb
@@ -2,6 +2,6 @@ require 'spec_helper'
 
 describe Contributor do
   it { is_expected.to belong_to :user }
-  it { is_expected.to belong_to :associator }
+  it { is_expected.to belong_to(:associator).class_name('User') }
   it { is_expected.to belong_to :target }
 end
--- spec/models/track_spec.rb
+++ spec/models/track_spec.rb
@@ -4,7 +4,7 @@ describe Track do
 
   describe "attributes" do
 
-    it "has title, description attributes" do
+    it "include title, description" do
       %w{title description created_at updated_at}.each do |field|
         expect(Track.attribute_names).to include field
       end
@@ -12,7 +12,7 @@ describe Track do
 
   end
 
-  it { is_expected.to belong_to :creator }
+  it { is_expected.to belong_to(:creator).class_name('User') }
 
   it "is creatable by any user" do
     expect(Track.creatable_by? build(:user)).to be true
