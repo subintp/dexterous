b08e9cb52c6b1dd88a0ca2fce4520336fdeb3f40
lorefnon <lorefnon@gmail.com>
2014-02-16 21:53:09 +0530

    :sparkles: rails g resource track

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Dexterous::Application.routes.draw do
+  resources :tracks
+
   devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
