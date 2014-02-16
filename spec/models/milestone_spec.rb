ece458e7bd31c489fb6a06e8425b485f45838115
lorefnon <lorefnon@gmail.com>
2014-02-17 02:23:53 +0530

    Test milestone attributes

--- spec/models/milestone_spec.rb
+++ spec/models/milestone_spec.rb
@@ -1,5 +1,12 @@
 require 'spec_helper'
 
 describe Milestone do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "attributes" do
+    it "include title, description created_at update_at" do
+      %w{title description created_at updated_at}.each do |field|
+        expect(Track.attribute_names).to include field
+      end
+    end
+  end
+
 end
