02bee8e3dadc5dd909ce08a8a5fdb25b317f4f32
lorefnon <lorefnon@gmail.com>
2014-02-16 21:22:54 +0530

    :sparkles: rails new dexterous -d mysql --skip-test-unit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Dexterous::Application.config.secret_key_base = 'fd3c73fd4bad6755db126931a854efb1284183aeda7263b16ea0f4c643b88ba1ee9670736e803be77e320266af4b97284731b6d98d8009f3c8442584c1907681'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Dexterous::Application.config.session_store :cookie_store, key: '_dexterous_session'
