f064cc1c1d9aac62d16bc49dbd9f9f201736b407
lorefnon <lorefnon@gmail.com>
2014-02-18 00:21:17 +0530

    :zap: :exclamation: Obliterate contributor
    An effort to pave the way for more elegant abstractions

--- spec/factories/contributors.rb
+++
@@ -1,8 +0,0 @@
-# Read about factories at https://github.com/thoughtbot/factory_girl
-
-FactoryGirl.define do
-  factory :track_contributor, class: 'Contributor' do
-    user
-    association :target, factory: :track
-  end
-end
