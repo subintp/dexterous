6c7a4bf95ef82582fbeea0c2dff3272a15719acf
lorefnon <lorefnon@gmail.com>
2014-02-17 23:08:23 +0530

    Automatically add milestone creator as a contributor

--- app/models/milestone.rb
+++ app/models/milestone.rb
@@ -4,4 +4,12 @@ class Milestone < ActiveRecord::Base
 
   belongs_to :track
   belongs_to :creator, class_name: 'User'
+
+  def add_creator_as_contributor
+    Contributor.create(
+      target: self,
+      user: creator,
+      is_admin: true
+    )
+  end
 end
