f60ca87b5950b32584f2f119a12de525563d1c7a
lorefnon <lorefnon@gmail.com>
2014-02-23 01:07:06 +0530

    :lipstick: The interface has started to look the way I want it to

--- app/controllers/tracks_controller.rb
+++ app/controllers/tracks_controller.rb
@@ -2,6 +2,16 @@ class TracksController < ApplicationController
   layout 'dashboard'
 
   def new
-    @track = Track.new
+    @track ||= Track.new
+  end
+
+  def create
+    @track = Track.new params
+      .require(:track)
+      .permit(:title, :description, :visibility)
+    @track.save!
+    redirect_to controller: 'home', action: 'dashboard'
+  rescue
+    render action: 'new'
   end
 end
