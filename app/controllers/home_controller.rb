96291fd4433462e1b224189466597434f4d78311
lorefnon <lorefnon@gmail.com>
2014-02-22 16:39:20 +0530

    Misc enhancements

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,8 @@ class HomeController < ApplicationController
   before_filter :authenticate_user!, only: ['dashboard']
 
   def dashboard
-    
+    @tracks = current_user.enrolled_tracks.includes :milestones
+    @new_track = Track.new
   end
 
 end
