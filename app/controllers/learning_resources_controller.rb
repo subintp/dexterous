135b2814cb6a15a892bfb6a1e4e635510453df9b
lorefnon <lorefnon@gmail.com>
2014-02-28 23:14:32 +0530

    :lipstick: Refine appearances

--- app/controllers/learning_resources_controller.rb
+++ app/controllers/learning_resources_controller.rb
@@ -1,2 +1,10 @@
-class LearningResourcesController < ApplicationController
+class LearningResourcesController < ResourceController
+  private
+
+  def extract_params
+    params
+      .require(:learning_resource)
+      .permit(:url, :title, :description, :metadata, :milestone_id, :track_id)
+  end
+
 end
