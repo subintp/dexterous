574a84ee560a221f46df7a2c8758e06e6b34620c
lorefnon <lorefnon@gmail.com>
2014-03-09 02:20:21 +0530

    Add some missing validations

--- app/models/enrollment.rb
+++ app/models/enrollment.rb
@@ -2,4 +2,6 @@ class Enrollment < ActiveRecord::Base
     belongs_to :user
     belongs_to :track
     has_many :achievements
+    
+    validates :user_id, :track_id, presence: true
 end
--- spec/models/enrollment_spec.rb
+++ spec/models/enrollment_spec.rb
@@ -4,4 +4,18 @@ describe Enrollment do
   it { is_expected.to belong_to :user }
   it { is_expected.to belong_to :track }
   it { is_expected.to have_many :achievements }
+  
+  it "validates presence of user_id" do
+    e = Enrollment.new
+    expect(e).to have(1).error_on(:user_id)
+    e.user_id = 1
+    expect(e).to have(0).error_on(:user_id)
+  end
+
+  it "validates presence of track_id" do
+    e = Enrollment.new
+    expect(e).to have(1).error_on(:track_id)
+    e.track_id = 1
+    expect(e).to have(0).error_on(:track_id)
+  end
 end
