2c46323f79ab45d9e4eeea6bac4483be563da147
lorefnon <lorefnon@gmail.com>
2014-03-09 01:54:51 +0530

    Minor bugfix

--- app/models/achievement.rb
+++ app/models/achievement.rb
@@ -1,5 +1,5 @@
 class Achievement < ActiveRecord::Base
     belongs_to :enrollment
-    belongs_to :user, through: :enrollment
+    has_one :user, through: :enrollment
     belongs_to :milestone
 end
