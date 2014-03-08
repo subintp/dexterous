c8bc942f0bceab05bead82cd7905022da02e6814
lorefnon <lorefnon@gmail.com>
2014-03-09 02:09:56 +0530

    Fix whitespace inconsistencies

--- spec/models/achievement_spec.rb
+++ spec/models/achievement_spec.rb
@@ -1,23 +1,23 @@
 require 'spec_helper'
 
 describe Achievement do
-  describe "attributes" do
-    it "include duration, score" do
-      %w{duration score}.each do |field|
-        expect(Achievement.attribute_names).to include field
-        expect(Achievement.column_names).to include field
-      end
+    describe "attributes" do
+        it "include duration, score" do
+            %w{duration score}.each do |field|
+                expect(Achievement.attribute_names).to include field
+                expect(Achievement.column_names).to include field
+            end
+        end
     end
-  end
 
-  it { is_expected.to have_one(:user)
-    .through(:enrollment)
-  }
-  it { is_expected.to belong_to(:milestone) }
-  it "validates presence of enrollment_id" do
-      expect(Achievement.new).to have(1).error_on(:enrollment_id)
-  end
-  it "validates presence of milestone_id" do
-      expect(Achievement.new).to have(1).error_on(:milestone_id)
-  end
+    it { is_expected.to have_one(:user)
+        .through(:enrollment)
+    }
+    it { is_expected.to belong_to(:milestone) }
+    it "validates presence of enrollment_id" do
+        expect(Achievement.new).to have(1).error_on(:enrollment_id)
+    end
+    it "validates presence of milestone_id" do
+        expect(Achievement.new).to have(1).error_on(:milestone_id)
+    end
 end
