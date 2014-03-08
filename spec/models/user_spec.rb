4e0a1cc4bdd2da1fc000a16036ef08e420e3a4b0
lorefnon <lorefnon@gmail.com>
2014-03-09 01:36:53 +0530

    Fix outdated specs for user

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,27 +1,32 @@
 require 'spec_helper'
 
 describe User do
-  describe "permissions" do
-  it { is_expected.to have_many :permissions }
 
-    %w{view edit contribute manage}.each do |field|
-      it { is_expected.to have_many :"#{field}_permissions" }
+    describe "permissions" do
+        it { is_expected.to have_many :permissions }
+         
+        %w{view edit contribute manage}.each do |field|
+            it { is_expected.to have_many :"#{field}_permissions" }
+        end
+         
+        %w{viewable editable contributable manageable}.each do |adj|
+            it { is_expected.to have_many :"#{adj}_tracks" }
+        end
     end
 
-    %w{viewable editable contributable manageable}.each do |adj|
-      it { is_expected.to have_many :"#{adj}_tracks" }
-    end
+    it { is_expected.to have_many(:tracks).through(:permissions) }
+    it { is_expected.to have_many(:owned_tracks) }
+    it { is_expected.to have_many :enrollments }
+    it { is_expected.to have_many(:achievements) 
+        .through(:enrollments)
+    }
+    it { is_expected.to have_many(:achieved_milestones)
+        .through(:achievements)
+        .source(:milestone)
+    }
+    it { is_expected.to have_many(:enrolled_tracks)
+        .through(:enrollments)
+        .source(:track)
+    }
 
-  end
-  it { is_expected.to have_many(:tracks).through(:permissions) }
-  it { is_expected.to have_many(:owned_tracks) }
-  it { is_expected.to have_many :enrollments }
-  it { is_expected.to have_many(:enrolled_tracks)
-      .through(:enrollments)
-      .source(:track)
-  }
-  it { is_expected.to have_many(:completed_milestones)
-      .through(:enrollments)
-      .source(:milestone)
-  }
 end
