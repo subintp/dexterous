18149dee0be715b39b5d64db3c6d409229840bf7
lorefnon <lorefnon@gmail.com>
2014-03-09 02:03:34 +0530

    Add missing validations

--- app/models/achievement.rb
+++ app/models/achievement.rb
@@ -2,4 +2,6 @@ class Achievement < ActiveRecord::Base
     belongs_to :enrollment
     has_one :user, through: :enrollment
     belongs_to :milestone
+    
+    validates :enrollment_id, :milestone_id, presence: true
 end
