caab0a077509f5c2546a2628de22074faa25d8dd
lorefnon <lorefnon@gmail.com>
2014-03-09 03:00:12 +0530

    Add missing validations for permission model

--- app/models/permission.rb
+++ app/models/permission.rb
@@ -1,4 +1,12 @@
 class Permission < ActiveRecord::Base
   belongs_to :user
   belongs_to :track
+
+  validates :user_id, :track_id, presence: true
+  validates :can_view,
+    inclusion: { in: [true]},
+    if: Proc.new {|p| p.can_edit }
+  validates :can_edit,
+    inclusion: { in: [true]},
+    if: Proc.new {|p| p.can_manage }
 end
--- spec/models/permission_spec.rb
+++ spec/models/permission_spec.rb
@@ -1,5 +1,29 @@
 require 'spec_helper'
 
 describe Permission do
-  pending "add some examples to (or delete) #{__FILE__}"
+    it { is_expected.to belong_to :user }
+    it { is_expected.to belong_to :track }
+
+    %w{user_id track_id}.each do |mandatory_field|
+        it "validates presence of #{mandatory_field}" do
+            p = Permission.new
+            expect(p).to have(1).error_on :"#{mandatory_field}"
+            p.send :"#{mandatory_field}=", 1
+            expect(p).to have(:no).error_on :"#{mandatory_field}"
+        end
+    end
+    
+    it "enforces view permission if edit permission is granted" do
+        p = Permission.new can_edit: true, can_view: false
+        expect(p).to have(1).error_on :can_view
+        p.can_view = true
+        expect(p).to have(:no).error_on :can_view
+    end
+    
+    it "enforces edit permission if manage permission is granted" do
+        p = Permission.new can_edit: false, can_manage: true
+        expect(p).to have(1).error_on :can_edit
+        p.can_edit = true
+        expect(p).to have(:no).error_on :can_edit
+    end
 end
