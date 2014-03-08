a3a33e943ecc477be60cea8d3361fd6e626d3c37
lorefnon <lorefnon@gmail.com>
2014-03-09 01:27:35 +0530

    Clean up code

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,44 +1,41 @@
 class User < ActiveRecord::Base
 
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+    # Include default devise modules. Others available are:
+    # :confirmable, :lockable, :timeoutable and :omniauthable
+    devise :database_authenticatable, :registerable,
+           :recoverable, :rememberable, :trackable, :validatable
 
-  include Authority::UserAbilities
+    include Authority::UserAbilities
 
-  has_many :permissions
-  has_many :tracks, through: :permissions
-  has_many :enrollments
-  has_many :achievements, through: :enrollments
-  has_many :enrolled_tracks,
-    through: :enrollments,
-    source: :track
+    has_many :permissions
+    has_many :tracks, through: :permissions
+    has_many :enrollments
+    has_many :achievements, through: :enrollments
+    has_many :enrolled_tracks,
+        through: :enrollments,
+        source: :track
 
-  {
-    view:       :viewable,
-    edit:       :editable,
-    contribute: :contributable,
-    manage:     :manageable
-  }.each do |verb, adjective|
+    adjective_map = {
+        view: :viewable,
+        edit: :editable,
+        contribute: :contributable,
+        manage: :manageable
+    }
+    adjective_map.each do |verb, adjective|
+        has_many :"#{verb}_permissions",
+            { class_name: 'Permission' },
+            -> { where(:"can_#{verb}" => true) }
 
-    has_many :"#{verb}_permissions", {
-        class_name: 'Permission'
-      }, -> { where(:"can_#{verb}" => true) }
+        has_many :"#{adjective}_tracks",
+            source: 'track',
+            through: :"#{verb}_permissions"
+    end
 
-    has_many :"#{adjective}_tracks",
-      source: 'track',
-      through: :"#{verb}_permissions"
-
-  end
-
-  %w{Track Milestone LearningResource}.each do |resource|
-
-    has_many :"owned_#{resource.underscore.pluralize}",
-      inverse_of: :owner,
-      foreign_key: :owner_id,
-      class_name: resource
-
-  end
+    %w{Track Milestone LearningResource}.each do |resource|
+        has_many :"owned_#{resource.underscore.pluralize}",
+            inverse_of: :owner,
+            foreign_key: :owner_id,
+            class_name: resource
+    end
 
 end
